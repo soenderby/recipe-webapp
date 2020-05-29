2ace0269bd7f2ee55e96972092b08a0922e82412
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2020-05-29 22:54:33 +0200

    Auto add emtpy ingredients

--- __tests__/lib/utils.test.js
+++ __tests__/lib/utils.test.js
@@ -1,5 +1,9 @@
 import { describe } from 'riteway';
-import { getEventValue } from '../../lib/utils';
+import {
+  getEventValue,
+  getLastElement,
+  addElementToList 
+} from '../../lib/utils';
 import { createRecipe } from '../testUtils/recipe-reducer-factories';
 
 const createEvent = (value) => ({ target: { value: value }});
@@ -12,6 +16,62 @@ describe('getEventValue', async assert => {
       should: 'return event.target.value',
       actual: getEventValue(event),
       expected: 'value'
+    });
+  }
+});
+
+describe('getLastElement', async assert => {
+  {
+    const list = [1, 2, 3, 4, 5]
+    assert({
+      given: 'array',
+      should: 'return the last element',
+      actual: getLastElement(list),
+      expected: 5
+    });
+  }
+
+  {
+    const list = ['first']
+    assert({
+      given: 'array with single element',
+      should: 'return the element',
+      actual: getLastElement(list),
+      expected: 'first'
+    });
+  }
+
+  {
+    const list = []
+    assert({
+      given: 'empty array',
+      should: 'return an empty list',
+      actual: getLastElement(list),
+      expected: []
+    });
+  }
+});
+
+describe('addElementToList', async assert => {
+  {
+    const element = 'element';
+    const list = [];
+    assert({
+      given: 'empty array',
+      should: 'return array with element',
+      actual: addElementToList(element, list),
+      expected: [...list, element]
+    });
+  }
+
+  {
+    const element = 'element';
+    const list = ['one', 'two', 'three'];
+    assert({
+      given: 'non empty array',
+      should: 'return array ending with given element',
+      actual: addElementToList(element, list),
+      expected: [...list, element]
     });
   }
 });
--- pages/create-recipe.js
+++ pages/create-recipe.js
@@ -10,8 +10,8 @@ import {
 import InstructionArea from '../components/createRecipeComponents/instructions-area';
 import IngredientList from '../components/createRecipeComponents/ingredient-list';
 import LabeledInput from '../components/createRecipeComponents/labeled-input';
-import { getEventValue } from '../lib/utils';
-import { compose } from 'ramda';
+import { getEventValue, getLastElement } from '../lib/utils';
+import { compose, curry, last } from 'ramda';
 
 const initialState = {
   name: '',
@@ -23,7 +23,16 @@ const initialState = {
 }
 
 export default () => {
-const [recipe, dispatch] = useReducer(reducer, initialState);
+  const [recipe, dispatch] = useReducer(reducer, initialState);
+
+  const addEmptyIngredient = (list) => {
+    const lastElement = getLastElement(list);
+    const { name, amount } = lastElement;
+    return name === '' && amount === '' ? 
+      list
+    :
+      dispatch(addIngredient({id: lastElement.id + 1, name: '', amount: ''}));
+  }
 
   return (
   <>
@@ -31,25 +40,32 @@ const [recipe, dispatch] = useReducer(reducer, initialState);
       labelText = { 'Recipe name: ' }
       inputText = { recipe.name }
       onChange = { text => 
-        compose(
-          dispatch,
-          changeName,
-          getEventValue,
-        )(text)
+          compose(
+            dispatch,
+            changeName,
+            getEventValue,
+          )(text)
       }
     />
     <InstructionArea
       text={ recipe.instructions }
       onChange={ text => 
-        compose(
-          dispatch,
-          addInstructions,
-          getEventValue,
-        )(text) }
+          compose(
+            dispatch,
+            addInstructions,
+            getEventValue,
+          )(text)
+      }
     />
     <IngredientList
       ingredients={ recipe.ingredients }
-      onChange={ (id, {name, amount}) => dispatch(changeIngredient(id, {name: name, amount: amount})) }
+      onChange={ 
+        (id, {name, amount}) => {
+          dispatch(changeIngredient(id, {name: name, amount: amount}));
+          // TODO: recipe is not updated here
+          addEmptyIngredient(recipe.ingredients);
+        }  
+      }
       onRemove={ id => dispatch(removeIngredient(id)) }
     />
     <button className='submit-button'>Submit Recipe</button>

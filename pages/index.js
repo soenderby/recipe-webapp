5e6b694e9d33de239024f1c1538d88a8caf40ed9
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2020-07-20 12:04:11 +0200

    Added babel plugin-transform-runtime to build and dev

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,18 @@ import React from 'react';
 import Link from 'next/link';
 import Layout from '../components/layout';
 
-export default function Home() {
-  return (
-    <Layout>
-      <div className="foo">Welcome to the Recipe Webapp!</div>
-      <br/>
-      <p>
-        Check out the 
-        <Link href="/create-recipe">
-          <a> create-recipe </a>
-        </Link> 
-        page.
-      </p>
-      
-      <p></p>
-    </Layout>
-  );
-}
+export default () =>
+  <Layout>
+    <div className="foo">Welcome to the Recipe Webapp!</div>
+    <br/>
+    <p>
+      Check out the 
+      <Link href="/create-recipe">
+        <a> create-recipe </a>
+      </Link> 
+      page.
+    </p>
+    
+    <p></p>
+  </Layout>
+;
